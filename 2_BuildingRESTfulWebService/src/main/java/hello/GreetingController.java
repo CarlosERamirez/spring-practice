2af618e4809ebc9b48d757dae9dbe2964e748a68
CarlosERamirez <carlose.ramirezsantiago@gmail.com>
2020-01-13 15:53:49 -0600

    Consuming a RESTful web service
    Creation of project with Spring Initializr

--- 2_BuildingRESTfulWebService/src/main/java/hello/GreetingController.java
+++ 2_BuildingRESTfulWebService/src/main/java/hello/GreetingController.java
@@ -5,13 +5,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+//This annotation marks the next class as a controller,
+//where every method returns a domain object intead of a view
 @RestController
 public class GreetingController {
 
   private static final String template = "Hello, %s!";
   private final AtomicLong counter = new AtomicLong();
-
+  
+  //RequestMapping ensures that HTTP requests to /greeting
+  //are mapped to the greeting() method
+  //RequestMapping is not specific to the GET request, it could
+  //be then (in this case) replaced by GetMapping
   @RequestMapping("/greeting")
+  //RequestParam binds the value of the query string parameter 'name'
+  //into the 'name' parameter of the greeting() method
   public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
     return new Greeting(counter.incrementAndGet(),
               String.format(template, name));
